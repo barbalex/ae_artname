11832a729b61672b3f3ed78ff8ed477cdff0529c
Alexander Gabriel <alex@gabriel-software.ch>
2016-10-25 20:29:21 +0200

    catch undefined art

--- src/setArtnameVollstaendig.js
+++ src/setArtnameVollstaendig.js
@@ -1,30 +1,33 @@
-/* eslint no-console:0, max-len:0, prefer-arrow-callback:0, func-names:0, no-underscore-dangle */
 'use strict'
 
+const _ = require(`lodash`)
+
 module.exports = (db, objects) => {
-  const savePromises = objects.map((object) => {
+  const objectsToSave = []
+  objects.forEach((object) => {
     if (object.Taxonomie && object.Taxonomie.Eigenschaften) {
       const eig = object.Taxonomie.Eigenschaften
       let artnameVollstaendig
       switch (object.Gruppe) {
         case `Flora`:
-          artnameVollstaendig = `${eig.Gattung} ${eig.Art}${eig.Autor ? ` ${eig.Autor}` : ``} ${eig.Art}${eig[`Unterart Name`] ? ` subsp. ${eig[`Unterart Name`]}${eig[`Unterart Autor`] ? ` ${eig[`Unterart Autor`]}` : ``}` : ``}${eig.NameDeutsch ? ` (${eig.NameDeutsch})` : ``}`
+          artnameVollstaendig = `${eig.Gattung}${eig.Art ? ` ${eig.Art}` : ``}${eig.Autor ? ` ${eig.Autor}` : ``}${eig[`Unterart Name`] ? ` subsp. ${eig[`Unterart Name`]}${eig[`Unterart Autor`] ? ` ${eig[`Unterart Autor`]}` : ``}` : ``}${eig.NameDeutsch ? ` (${eig.NameDeutsch})` : ``}`
           break
         case `Fauna`:
         case `Moose`:
-          artnameVollstaendig = `${eig.Gattung} ${eig.Art}${eig.Autor ? ` ${eig.Autor}` : ``} ${eig.Art}${eig.Unterart ? ` subsp. ${eig.Unterart}${eig[`Unterart Autor`] ? ` ${eig[`Unterart Autor`]}` : ``}` : ``}${eig.NameDeutsch ? ` (${eig.NameDeutsch})` : ``}`
+          artnameVollstaendig = `${eig.Gattung}${eig.Art ? ` ${eig.Art}` : ``}${eig.Autor ? ` ${eig.Autor}` : ``}${eig.Unterart ? ` subsp. ${eig.Unterart}${eig[`Unterart Autor`] ? ` ${eig[`Unterart Autor`]}` : ``}` : ``}${eig.NameDeutsch ? ` (${eig.NameDeutsch})` : ``}`
           break
         case `Macromycetes`:
-          artnameVollstaendig = `${eig.Gattung} ${eig.Art}${eig.Autor ? ` ${eig.Autor}` : ``}${eig.NameDeutsch ? ` (${eig.NameDeutsch})` : ``}`
+          artnameVollstaendig = `${eig.Gattung}${eig.Art ? ` ${eig.Art}` : ``}${eig.Autor ? ` ${eig.Autor}` : ``}${eig.NameDeutsch ? ` (${eig.NameDeutsch})` : ``}`
           break
         default:
-          artnameVollstaendig = `${eig.Gattung} ${eig.Art}${eig.Autor ? ` ${eig.Autor}` : ``}${eig.NameDeutsch ? ` (${eig.NameDeutsch})` : ``}`
+          artnameVollstaendig = `${eig.Gattung}${eig.Art ? ` ${eig.Art}` : ``}${eig.Autor ? ` ${eig.Autor}` : ``}${eig.NameDeutsch ? ` (${eig.NameDeutsch})` : ``}`
       }
-      object.Taxonomie.Eigenschaften[`Artname vollständig`] = artnameVollstaendig
-      return db.saveAsync(object._id, object._rev, object)
+      if (object.Taxonomie.Eigenschaften[`Artname vollständig`] && object.Taxonomie.Eigenschaften[`Artname vollständig`] !== artnameVollstaendig) {
+        object.Taxonomie.Eigenschaften[`Artname vollständig`] = artnameVollstaendig
+        objectsToSave.push(object)
+      }
     }
-    return new Error(`no object.Taxonomie.Eigenschaften found for object: ${object}`)
   })
-
-  return Promise.all(savePromises)
+  const objectArraysToSave = _.chunk(objectsToSave, 20)
+  return Promise.all(objectArraysToSave.map(objectArray => db.saveAsync(objectArray)))
 }
