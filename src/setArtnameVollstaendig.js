bae202639d1c113d9b76dd4a624b3616e2968f34
Alexander Gabriel <alex@gabriel-software.ch>
2016-10-25 17:19:24 +0200

    work on it

--- src/setArtnameVollstaendig.js
+++ src/setArtnameVollstaendig.js
@@ -1,46 +1,30 @@
-/* eslint no-console:0, max-len:0, prefer-arrow-callback:0, func-names:0 */
+/* eslint no-console:0, max-len:0, prefer-arrow-callback:0, func-names:0, no-underscore-dangle */
 'use strict'
 
-module.exports = (db, objects) =>
-  new Promise((resolve, reject) => {
-    const objectsToSave = []
-
-    objects.forEach((object) => {
-      if (object.Taxonomie && object.Taxonomie.Eigenschaften) {
-        const eig = object.Taxonomie.Eigenschaften
-        let artnameVollstaendig
-        switch (object.Gruppe) {
-          case 'Flora':
-            artnameVollstaendig = `${eig.Gattung} ${eig.Art}${eig.Autor ? ` ${eig.Autor}` : ''} ${eig.Art}${eig['Unterart Name'] ? ` subsp. ${eig['Unterart Name']}${eig['Unterart Autor'] ? ` ${eig['Unterart Autor']}` : ''}` : ''}${eig.NameDeutsch ? ` (${eig.NameDeutsch})` : ''}`
-            break
-          case 'Fauna':
-          case 'Moose':
-            artnameVollstaendig = `${eig.Gattung} ${eig.Art}${eig.Autor ? ` ${eig.Autor}` : ''} ${eig.Art}${eig.Unterart ? ` subsp. ${eig.Unterart}${eig['Unterart Autor'] ? ` ${eig['Unterart Autor']}` : ''}` : ''}${eig.NameDeutsch ? ` (${eig.NameDeutsch})` : ''}`
-            break
-          case 'Macromycetes':
-            artnameVollstaendig = `${eig.Gattung} ${eig.Art}${eig.Autor ? ` ${eig.Autor}` : ''}${eig.NameDeutsch ? ` (${eig.NameDeutsch})` : ''}`
-            break
-          default:
-            artnameVollstaendig = `${eig.Gattung} ${eig.Art}${eig.Autor ? ` ${eig.Autor}` : ''}${eig.NameDeutsch ? ` (${eig.NameDeutsch})` : ''}`
-        }
-        object.Taxonomie.Eigenschaften['Artname vollständig'] = artnameVollstaendig
-        objectsToSave.push(object)
-      } else {
-        console.error('no object.Taxonomie.Eigenschaften found for object:', object)
+module.exports = (db, objects) => {
+  const savePromises = objects.map((object) => {
+    if (object.Taxonomie && object.Taxonomie.Eigenschaften) {
+      const eig = object.Taxonomie.Eigenschaften
+      let artnameVollstaendig
+      switch (object.Gruppe) {
+        case `Flora`:
+          artnameVollstaendig = `${eig.Gattung} ${eig.Art}${eig.Autor ? ` ${eig.Autor}` : ``} ${eig.Art}${eig[`Unterart Name`] ? ` subsp. ${eig[`Unterart Name`]}${eig[`Unterart Autor`] ? ` ${eig[`Unterart Autor`]}` : ``}` : ``}${eig.NameDeutsch ? ` (${eig.NameDeutsch})` : ``}`
+          break
+        case `Fauna`:
+        case `Moose`:
+          artnameVollstaendig = `${eig.Gattung} ${eig.Art}${eig.Autor ? ` ${eig.Autor}` : ``} ${eig.Art}${eig.Unterart ? ` subsp. ${eig.Unterart}${eig[`Unterart Autor`] ? ` ${eig[`Unterart Autor`]}` : ``}` : ``}${eig.NameDeutsch ? ` (${eig.NameDeutsch})` : ``}`
+          break
+        case `Macromycetes`:
+          artnameVollstaendig = `${eig.Gattung} ${eig.Art}${eig.Autor ? ` ${eig.Autor}` : ``}${eig.NameDeutsch ? ` (${eig.NameDeutsch})` : ``}`
+          break
+        default:
+          artnameVollstaendig = `${eig.Gattung} ${eig.Art}${eig.Autor ? ` ${eig.Autor}` : ``}${eig.NameDeutsch ? ` (${eig.NameDeutsch})` : ``}`
       }
-    })
-
-    if (objectsToSave.length === 0) {
-      console.log('0 names updated')
-      return resolve()
+      object.Taxonomie.Eigenschaften[`Artname vollständig`] = artnameVollstaendig
+      return db.saveAsync(object._id, object._rev, object)
     }
-    db.saveAsync(objectsToSave)
-      .then(() => {
-        console.log(`${objectsToSave.length} names updated`)
-        resolve()
-      })
-      .catch((error) => {
-        console.error('insertNewCscfProperties error:', error)
-        reject()
-      })
+    return new Error(`no object.Taxonomie.Eigenschaften found for object: ${object}`)
   })
+
+  return Promise.all(savePromises)
+}
