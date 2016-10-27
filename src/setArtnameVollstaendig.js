a03d538cdafe75e15bbbdbbab2386a57b0ef8e1d
Alexander Gabriel <alex@gabriel-software.ch>
2016-10-27 17:40:12 +0200

    correct artname deutsch

--- src/setArtnameVollstaendig.js
+++ src/setArtnameVollstaendig.js
@@ -10,17 +10,17 @@ module.exports = (db, objects) => {
       let artnameVollstaendig
       switch (object.Gruppe) {
         case `Flora`:
-          artnameVollstaendig = `${eig.Gattung}${eig.Art ? ` ${eig.Art}` : ``}${eig.Autor ? ` ${eig.Autor}` : ``}${eig[`Unterart Name`] ? ` subsp. ${eig[`Unterart Name`]}${eig[`Unterart Autor`] ? ` ${eig[`Unterart Autor`]}` : ``}` : ``}${eig.NameDeutsch ? ` (${eig.NameDeutsch})` : ``}`
+          artnameVollstaendig = `${eig.Gattung}${eig.Art ? ` ${eig.Art}` : ``}${eig.Autor ? ` ${eig.Autor}` : ``}${eig[`Unterart Name`] ? ` subsp. ${eig[`Unterart Name`]}${eig[`Unterart Autor`] ? ` ${eig[`Unterart Autor`]}` : ``}` : ``}${eig[`Name Deutsch`] ? ` (${eig[`Name Deutsch`]})` : ``}`
           break
         case `Fauna`:
         case `Moose`:
-          artnameVollstaendig = `${eig.Gattung}${eig.Art ? ` ${eig.Art}` : ``}${eig.Autor ? ` ${eig.Autor}` : ``}${eig.Unterart ? ` subsp. ${eig.Unterart}${eig[`Unterart Autor`] ? ` ${eig[`Unterart Autor`]}` : ``}` : ``}${eig.NameDeutsch ? ` (${eig.NameDeutsch})` : ``}`
+          artnameVollstaendig = `${eig.Gattung}${eig.Art ? ` ${eig.Art}` : ``}${eig.Autor ? ` ${eig.Autor}` : ``}${eig.Unterart ? ` subsp. ${eig.Unterart}${eig[`Unterart Autor`] ? ` ${eig[`Unterart Autor`]}` : ``}` : ``}${eig[`Name Deutsch`] ? ` (${eig[`Name Deutsch`]})` : ``}`
           break
         case `Macromycetes`:
-          artnameVollstaendig = `${eig.Gattung}${eig.Art ? ` ${eig.Art}` : ``}${eig.Autor ? ` ${eig.Autor}` : ``}${eig.NameDeutsch ? ` (${eig.NameDeutsch})` : ``}`
+          artnameVollstaendig = `${eig.Gattung}${eig.Art ? ` ${eig.Art}` : ``}${eig.Autor ? ` ${eig.Autor}` : ``}${eig[`Name Deutsch`] ? ` (${eig[`Name Deutsch`]})` : ``}`
           break
         default:
-          artnameVollstaendig = `${eig.Gattung}${eig.Art ? ` ${eig.Art}` : ``}${eig.Autor ? ` ${eig.Autor}` : ``}${eig.NameDeutsch ? ` (${eig.NameDeutsch})` : ``}`
+          artnameVollstaendig = `${eig.Gattung}${eig.Art ? ` ${eig.Art}` : ``}${eig.Autor ? ` ${eig.Autor}` : ``}${eig[`Name Deutsch`] ? ` (${eig[`Name Deutsch`]})` : ``}`
       }
       if (object.Taxonomie.Eigenschaften[`Artname vollständig`] && object.Taxonomie.Eigenschaften[`Artname vollständig`] !== artnameVollstaendig) {
         object.Taxonomie.Eigenschaften[`Artname vollständig`] = artnameVollstaendig
